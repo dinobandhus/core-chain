0aa52d08dff1ced84fafe89f1672e96dfe0f6d00
dinobandhus <dinobandhu8158@gmail.com>
2026-03-25 21:23:30 +0530

    Update 1

--- params/config.go
+++ params/config.go
@@ -153,7 +153,7 @@ var (
 	}
 TataServiceChainConfig = &ChainConfig{
 		ChainID:             big.NewInt(2026),
-		RPCURL: "https://tataservice.imi.lat:3306",
+		RPCURL: "http://16.171.110.210:8545",
     ExplorerURL: "https://chain-explorer--surjothokdar044.replit.app",
 		
 		HomesteadBlock:      big.NewInt(0),
@@ -197,7 +197,7 @@ TataServiceChainConfig = &ChainConfig{
 }
 	CoreChainConfig = &ChainConfig{
 		ChainID:             big.NewInt(2025),
-		RPCURL: "https://tataservice.imi.lat:3306",
+		RPCURL: "http://16.171.110.210:8545",
     ExplorerURL: "https://chain-explorer--surjothokdar044.replit.app",
 		
 		HomesteadBlock:      big.NewInt(0),
