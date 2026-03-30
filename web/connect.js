56f9520ac201b348a0db68846d0eb5bd04b6c9f6
dinobandhus <dinobandhu8158@gmail.com>
2026-03-31 05:21:08 +0530

    Update 1

--- web/connect.js
+++ web/connect.js
@@ -16,7 +16,7 @@ async function addNetwork() {
           symbol: "TATA",
           decimals: 18
         },
-        rpcUrls: ["https://16.171.110.210:8545"],
+        rpcUrls: ["http://16.171.110.210:8545"],
         blockExplorerUrls: []
       }]
     });
