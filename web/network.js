946c0dc1b1e724cd108a2908fcbe37e8089edb6f
dinobandhus <dinobandhu8158@gmail.com>
2026-04-05 21:20:22 +0530

    Update 2

--- web/network.js
+++ web/network.js
@@ -2,5 +2,5 @@ export const network = {
       name: "Tata Service",
         symbol: "TATA",
           rpc: "http://16.171.110.210:8545",
-            chainId: 2026
+            chainId: 2601
             };
